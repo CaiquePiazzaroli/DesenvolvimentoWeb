9115db48b1ab69d5e3e43e78256de4451fa0c42c
Caique Piazzaroli <caique405@gmail.com>
2024-11-26 14:07:52 -0300

    Ajuste carrosel 3

---
+++ Projetos/trabalho_web_semestral/scripts/carousel.js
@@ -0,0 +1,29 @@
+
+let buttonLeft = document.querySelector(".carousel_button_left");
+let buttonRight = document.querySelector(".carousel_button_right");
+let carousel = document.querySelector(".carousel");
+
+let switchToLeft = () => {
+    carousel.scrollBy({
+        top: 0,
+        left: 1,
+        behavior: "smooth",
+        });
+}
+
+let switchToRight = () => {
+    carousel.scrollBy({
+        top: 0,
+        left: -1,
+        behavior: "smooth",
+    });
+}
+
+buttonLeft.addEventListener("click", () => {
+    switchToRight();
+})
+
+buttonRight.addEventListener("click", () => {
+    switchToLeft();
+})
+
